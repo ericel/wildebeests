db5c21e6328d7905793c07f29f413be7842c1989
ericel <ericel123new@gmail.com>
2017-12-21 14:27:09 -0800

    Blog get Id router string

--- src/app/public/content/blogs/blog/blog.component.ts
+++ src/app/public/content/blogs/blog/blog.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavbarService } from '../../../../shared/core/navbar/navbar.service';
 import { Title, Meta } from '@angular/platform-browser';
+import { ActivatedRoute, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 import * as actions from './../state/blogs.actions';
@@ -12,10 +13,20 @@ import * as fromBlog from './../state/blogs.reducer';
 })
 export class BlogComponent implements OnInit {
 
-  constructor(public nav: NavbarService, private title: Title, private meta: Meta) { }
+  constructor(public nav: NavbarService,
+     private title: Title,
+    private meta: Meta,
+    private router: Router,
+    private route: ActivatedRoute,) { }
   
     ngOnInit() {
       this.nav.show();
+        this.route.params.subscribe(params => {
+        let str = params['string'];
+        let n = str.lastIndexOf('-');
+        let result = str.substring(n + 1);
+        console.log(result)
+      });
       this.title.setTitle('Blog name');
       this.meta.addTags([
         { name: 'keywords', content: 'Blog Editor'},
